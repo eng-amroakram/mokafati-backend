686b5b238341626647e1108e7ac26deba94dfe8a
eng-amroakram <eng.amroakram@gmail.com>
2025-03-11 12:52:31 +0200

    new update

--- resources/views/livewire/panel/index.blade.php
+++ resources/views/livewire/panel/index.blade.php
@@ -3,7 +3,7 @@
         <a class="col-xl-3 col-md-6 mb-5" href="{{ route('admin.panel.users') }}">
             <div class="card card-cascade cascading-admin-card">
                 <div class="admin-up">
-                    <i class="fas fa-users green-color mr-3 z-depth-2"></i>
+                    <i class="fas fa-users blue-color mr-3 z-depth-2"></i>
                     <div class="data">
                         <p class="text-uppercase fs-6 fw-bold">المشتركين</p>
                         <h4 class="font-weight-bold text-dark">{{ models_count('User') }}</h4>
@@ -15,7 +15,7 @@
         <a class="col-xl-3 col-md-6 mb-5" href="{{ route('admin.panel.stores') }}">
             <div class="card card-cascade cascading-admin-card">
                 <div class="admin-up">
-                    <i class="fas fa-store green-color mr-3 z-depth-2"></i>
+                    <i class="fas fa-store blue-color mr-3 z-depth-2"></i>
                     <div class="data">
                         <p class="text-uppercase fs-6 fw-bold">المتاجر</p>
                         <h4 class="font-weight-bold text-dark">{{ models_count('Store') }}</h4>
@@ -27,7 +27,7 @@
         <a class="col-xl-3 col-md-6 mb-5" href="#">
             <div class="card card-cascade cascading-admin-card">
                 <div class="admin-up">
-                    <i class="fas fa-user-tag green-color mr-3 z-depth-2"></i>
+                    <i class="fas fa-user-tag blue-color mr-3 z-depth-2"></i>
                     <div class="data">
                         <p class="text-uppercase fs-6 fw-bold">موظفين المتاجر</p>
                         <h4 class="font-weight-bold text-dark">{{ models_count('Employee') }}</h4>
@@ -41,7 +41,7 @@
         <a class="col-xl-3 col-md-6 mb-5" href="{{ route('admin.panel.packages') }}">
             <div class="card card-cascade cascading-admin-card">
                 <div class="admin-up">
-                    <i class="fas fa-cubes green-color mr-3 z-depth-2"></i>
+                    <i class="fas fa-cubes blue-color mr-3 z-depth-2"></i>
                     <div class="data">
                         <p class="text-uppercase fs-6 fw-bold">الباقات</p>
                         <h4 class="font-weight-bold text-dark">{{ models_count('Package') }}</h4>
